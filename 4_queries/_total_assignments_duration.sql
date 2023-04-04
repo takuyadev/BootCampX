2074652fb9eb6bb8d06ea8579a67d0a8d9e2f216
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-04 14:10:53 -0700

    Finish counting assistances

--- 4_queries/_total_assignments_duration.sql
+++ 4_queries/_total_assignments_duration.sql
@@ -1,11 +1,11 @@
-SELECT teachers.name FROM assistance_requests 
+SELECT teachers.name, students.cohort_name FROM assistance_requests 
 LEFT JOIN teachers ON (teachers.id=assistance_requests.teacher_id)
 JOIN (
-  SELECT cohorts.name, students.id AS id FROM students
+  SELECT cohorts.name AS cohort_name, students.id AS id FROM students
   JOIN cohorts ON (students.cohort_id=cohorts.id)
   WHERE cohorts.name = 'JUL02'
 ) as students ON (students.id = assistance_requests.student_id)
-GROUP BY teachers.name
+GROUP BY teachers.name, students.cohort_name
 ORDER BY teachers.name ASC;
 
 
