b8deb3f4184c7cf74813e9cd64149ca3b34c5ffa
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-04 09:43:06 -0700

    Query for total assistance requests from teacher

--- 4_queries/_total_assistance_requests.sql
+++ 4_queries/_total_assistance_requests.sql
@@ -1,2 +1,4 @@
-SELECT COUNT(id) AS total_assistances, teachers.name FROM teachers
+SELECT count(assistance_requests.*) AS total_assistances, teachers.name FROM assistance_requests
+ JOIN teachers ON (teachers.id=assistance_requests.teacher_id)
+ WHERE teachers.name= 'Waylon Boehm'
  GROUP BY teachers.name;
