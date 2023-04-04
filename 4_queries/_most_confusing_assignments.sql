5b0d39f2ee52e3f5103dc80019e4b10e1c5305e2
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-04 13:34:29 -0700

    Finish total assignments assigned

--- 4_queries/_most_confusing_assignments.sql
+++ 4_queries/_most_confusing_assignments.sql
@@ -1,4 +1,4 @@
-SELECT b.id, b.name, b.day, b.chapter, SUM(assignment_id) AS total_requests FROM assistance_requests AS a
+SELECT b.id, b.name, b.day, b.chapter, COUNT(b.id) AS total_requests FROM assistance_requests AS a
 JOIN assignments AS b ON (b.id=a.assignment_id)
 GROUP BY b.id, b.name, b.day, b.chapter
 ORDER BY total_requests DESC;
---
+++ 4_queries/_total_assignments_duration.sql
@@ -0,0 +1,3 @@
+SELECT day, COUNT(*) AS number_of_assignments, SUM(duration) AS duration FROM assignments
+GROUP BY day
+ORDER BY day ASC;
